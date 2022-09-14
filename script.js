2b501375a1939da69bf6eae714628e9a6183ae96
moses <leecmoses@gmail.com>
2022-09-14 11:40:26 -0400

    Add event listeners to input buttons

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const calculatorDisplay = document.querySelector("h1");
+const inputBtns = document.querySelectorAll("button");
+const clearBtn = document.getElementById("clear-btn");
+
+const sendNumVal = (num) => {
+  // If cur display val is 0, replace it, if not concat number
+  const displayVal = calculatorDisplay.textContent;
+  calculatorDisplay.textContent = displayVal === "0" ? num : displayVal + num;
+};
+
+// Add Event Listeners for numbers, operators, decimal buttons
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener("click", () => sendNumVal(inputBtn.value));
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => sendNumVal(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => sendNumVal(inputBtn.value));
+  }
+});

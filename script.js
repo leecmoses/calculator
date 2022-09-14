b47b1e9794a1502d7115e383a2974f3038fd6e0d
moses <leecmoses@gmail.com>
2022-09-14 13:16:53 -0400

    Write the calculate functionality

--- script.js
+++ script.js
@@ -30,18 +30,36 @@ const addDecimal = () => {
     : calculatorDisplay.textContent + ".";
 };
 
+// Calculate first and second caludes depending on operator
+const calculate = {
+  "/": (firstNumber, secondNumber) => firstNumber / secondNumber,
+  "*": (firstNumber, secondNumber) => firstNumber * secondNumber,
+  "+": (firstNumber, secondNumber) => firstNumber + secondNumber,
+  "-": (firstNumber, secondNumber) => firstNumber - secondNumber,
+  "=": (firstNumber, secondNumber) => secondNumber,
+};
+
 const useOperator = (operator) => {
   const currentValue = Number(calculatorDisplay.textContent);
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+    return;
+  }
 
   // Assign firstValue if no value
-  !firstValue
-    ? (firstValue = currentValue)
-    : console.log("currentValue", currentValue);
+
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
+  }
 
   // Ready for next value, store operator
   awaitingNextValue = true;
   operatorValue = operator;
-  console.table(firstValue, operatorValue);
 };
 
 // Add Event Listeners for numbers, operators, decimal buttons
